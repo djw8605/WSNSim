a7c3b90c5c9932914256c573e9e7e40fb18d885b
Derek Weitzel <djw8605@gmail.com>
2011-11-26 12:05:18 -0600

    Adding generator

---
+++ src/wsnsim/generator.py
@@ -0,0 +1,18 @@
+
+
+import re, sys, optparse
+
+def AddOptions(parser):
+    
+
+def main():
+    
+    parser = optparse.OptionParser()
+    AddOptions(parser)
+    
+
+
+
+if __name__ == "__main__":
+    main()
+
