77653f04ebdf28aa8aafb4cf783642cd465698d4
Derek Weitzel <djw8605@gmail.com>
2011-11-26 12:20:27 -0600

    Adding generator file

---
+++ src/wsnsim/GeneratorFile.py
@@ -0,0 +1,13 @@
+
+import re
+
+
+class GeneratorFile:
+    def __init__(self, filename):
+        self.filename = filename
+        self.
+        
+        
+    def parse(self):
+        
+
--- src/wsnsim/generator.py
+++ src/wsnsim/generator.py
@@ -2,13 +2,18 @@
 
 import re, sys, optparse
 
+import GeneratorFile
+
 def AddOptions(parser):
-    
+    parser.add_option("-f", "--file", dest="filename", 
+                      help="File to read the simulation configuration")
+
 
 def main():
     
     parser = optparse.OptionParser()
     AddOptions(parser)
+    
     
 
 
