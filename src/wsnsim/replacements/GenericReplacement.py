703c77294eae21db236254d0ef27986ac6d4097f
Derek Weitzel <djw8605@gmail.com>
2011-11-26 18:37:42 -0600

    Adding GenericReplacement superclass

---
+++ src/wsnsim/replacements/GenericReplacement.py
@@ -0,0 +1,13 @@
+
+
+
+class GenericReplacement:
+    def __init__(self):
+        pass
+    
+    def GetNextValue(self):
+        return 0
+    
+    def GetNumberOfValues(self):
+        return 0
+
