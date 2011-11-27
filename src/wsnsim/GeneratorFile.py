d86aaab2f7729aadf17aeaa57dc90051b9cde0f5
Derek Weitzel <djw8605@gmail.com>
2011-11-26 20:45:52 -0600

    Adding simfile and working writing files

--- src/wsnsim/GeneratorFile.py
+++ src/wsnsim/GeneratorFile.py
@@ -2,10 +2,12 @@
 import re, ConfigParser
 import replacements
 from replacements import GetReplacement
+import os
 
 class GeneratorFile:
     def __init__(self, filename):
         self.filename = filename
+        self._filecounter = 0
         #self.iterate_re = re.compile("(\w+)\s*=\s*\[([[\d]+\.[\d]+]|[\d+])..([[\d]+\.[\d]+]|[\d+])\] step ([[\d]+\.[\d]+]|[\d+])")
         
         #self.iterate_re = re.compile("(\w+)\s*=\s*\[([\d]*\.{0,1}[\d]*)..([\d]*\.{0,1}[\d]*)\]\s+step\s+([\d]*\.{0,1}[\d]*)")
@@ -26,18 +28,21 @@ class GeneratorFile:
         
         
     def writeSimFile(self, simfile, destinationdir):
+        f = open(simfile)
+        simfile_string = f.read()
         params_names = self.params.keys()
         not_seen_list = params_names[1:]
         current_param = params_names[0]
         for i in range(self.params[params_names[0]].GetNumberOfValues()):
             seen_dict = {current_param: self.params[current_param].GetNextValue()}
-            self._recursive_traverse(seen_dict, not_seen_list, simfile, destinationdir)
+            self._recursive_traverse(seen_dict, not_seen_list, simfile_string, destinationdir)
         
         
-    def _recursive_traverse(self, seen_dict, not_seen_list, simfile, destinationdir):
+    def _recursive_traverse(self, seen_dict, not_seen_list, simfile_string, destinationdir):
         
         if len(not_seen_list) == 0:
             # Write out the file.
+            self.WriteFile(simfile_string, seen_dict, destinationdir)
             print seen_dict
             return
         
@@ -45,5 +50,20 @@ class GeneratorFile:
         current_param = not_seen_list[0]
         for i in range(self.params[current_param].GetNumberOfValues()):
             seen_dict[current_param] = self.params[current_param].GetNextValue()
-            self._recursive_traverse(seen_dict, new_not_seen_list, simfile, destinationdir)
+            self._recursive_traverse(seen_dict, new_not_seen_list, simfile_string, destinationdir)
+            
+            
+    def WriteFile(self, simfile_string, replace_dict, destinationdir):
+        self._filecounter += 1
+        output_filename = os.path.join(destinationdir, "submit%i.simconfig" % self._filecounter)
+        print output_filename
+        f = open(output_filename, 'w')
+        f.write(simfile_string % replace_dict)
+        f.close()
+        
+        
+        
+            
+            
+            
         
