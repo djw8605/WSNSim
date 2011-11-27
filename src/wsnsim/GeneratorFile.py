cf3e3a6e2b661535e633bff0dbe2d33bc15b68be
Derek Weitzel <djw8605@gmail.com>
2011-11-26 18:15:02 -0600

    More changes after adding replacements

--- src/wsnsim/GeneratorFile.py
+++ src/wsnsim/GeneratorFile.py
@@ -1,17 +1,23 @@
 
-import re
-
+import re, ConfigParser
+import replacements
+from replacements import GetReplacement
 
 class GeneratorFile:
     def __init__(self, filename):
         self.filename = filename
-        self.iterate_re = re.compile("(\w+)\s*=\s*\[([[\d]+\.[\d]+]|[\d+])..([[\d]+\.[\d]+]|[\d+])\] step ([[\d]+\.[\d]+]|[\d+])")
-        self.iterate_re = re.compile("(\w+)\s*=\s*\[([\d]*\.{0,1}[\d]*)..([\d]*\.{0,1}[\d]*)\]\s+step\s+([\d]*\.{0,1}[\d]*)")
+        #self.iterate_re = re.compile("(\w+)\s*=\s*\[([[\d]+\.[\d]+]|[\d+])..([[\d]+\.[\d]+]|[\d+])\] step ([[\d]+\.[\d]+]|[\d+])")
+        
+        #self.iterate_re = re.compile("(\w+)\s*=\s*\[([\d]*\.{0,1}[\d]*)..([\d]*\.{0,1}[\d]*)\]\s+step\s+([\d]*\.{0,1}[\d]*)")
         
     def parse(self):
-        f = open(self.filename)
-        for line in f.readlines():
-            iterate_match = self.iterate_re.search(line)
-            if iterate_match:
-                print "Found iterate"
-                print iterate_match.groups()
+        # Configuration parameters
+        self.params = {}
+        
+        parser = ConfigParser.ConfigParser()
+        parser.read(self.filename)
+        config_params = parser.items("main")
+        print config_params
+        
+        for (param_name, param_value) in config_params:
+            self.params[param_name] = GetReplacement(param_value)
